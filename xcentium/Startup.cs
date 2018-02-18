96c9bcc67eec4a0c948f89d6c6806756782cf22b
mchavez <mchavez@MACHODEV1>
2018-02-18 19:42:22 +0000

    Add project files.

---
+++ xcentium/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(xcentium.Startup))]
+namespace xcentium
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
